936b48cc98e947d95c0105c03dcf636290afa715
caco123 <willian.galvez91@gmail.com>
2024-11-07 22:42:58 +0100

    creando nuevos esquemas

---
+++ projects/dummy-ui-lib/schematics/dummy-component/index.ts
@@ -0,0 +1,53 @@
+import {
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  Tree,
+} from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { Observable } from 'rxjs';
+import { applyToUpdateRecorder } from '@schematics/angular/utility/change';
+import { addImportToModule } from '@schematics/angular/utility/ast-utils';
+import * as ts from 'typescript';
+
+export function dummyComponentGenerator(): Rule {
+  return (
+    tree: Tree,
+    context: SchematicContext
+  ): Tree | Observable<Tree> | Rule | Promise<void | Rule> | void => {
+    context.logger.info('Adding Modules to the App');
+    const modulePath = '/src/app/app.module.ts';
+
+    if (!tree.exists(modulePath)) {
+      throw new SchematicsException(`The file ${modulePath} doesn't exists`);
+    }
+
+    const text = tree.read(modulePath);
+    if (text === null) {
+      throw new SchematicsException(`The file ${modulePath} doesn't exists`);
+    }
+
+    const sourceFile = ts.createSourceFile(
+      modulePath,
+      text.toString(),
+      ts.ScriptTarget.Latest,
+      true
+    );
+
+    const recorder = tree.beginUpdate(modulePath);
+    applyToUpdateRecorder(
+      recorder,
+      addImportToModule(
+        sourceFile,
+        modulePath,
+        'DummyUiLibModule',
+        'dummy-ui-lib'
+      )
+    );
+    tree.commitUpdate(recorder);
+
+    context.logger.info('Installing dependencies..');
+    context.addTask(new NodePackageInstallTask({}));
+    return tree;
+  };
+}
